30157a429dc28f5f2d0e7aba927df266c5b25eb9
ragnarok2022 <j7samsungek@gmail.com>
2023-08-29 16:55:40 -0300

    Sistema de Alugues

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js}',
+    './src/components/**/*.{js}',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
